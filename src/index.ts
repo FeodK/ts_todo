0b55e5d046cd3cd07926bcc086324f7eaff0d67e
Feodor <mr.fedor.kiselev@gmail.com>
2024-11-18 12:50:18 +0300

    first

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,89 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidv4 } from 'uuid';
 
-import confetti from 'canvas-confetti';
+type Task = {
+  id: string,
+  title: string,
+  completed: boolean,
+  createdAt: Date,
+}
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.querySelector<HTMLUListElement>('#list');
+const form = document.querySelector<HTMLFormElement>('#task-form');
+const input = document.querySelector<HTMLInputElement>('#task-input');
+
+const tasks: Task[] = loadTasks();
+
+tasks.forEach(addListItem);
+
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (input?.value == '' || input?.value == null) return;
+
+  const task: Task = {
+    id: uuidv4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+
+  tasks.push(task);
+  saveTasks();
+
+  addListItem(task);
+
+  input.value = '';
+})
+
+function addListItem(task: Task) {
+  const item = document.createElement('li');
+  item.classList.add('task-item');
+
+  if (task.completed) {
+    item.classList.add('completed');
+  }
+
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked;
+    saveTasks();
+
+    if (checkbox.checked) {
+      item.classList.add('completed');
+    } else {
+      item.classList.remove('completed');
+    }
+  });
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed;
+  label.append(checkbox, task.title);
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'X';
+  deleteButton.addEventListener('click', () => {
+    deleteTask(task.id);
+    item.remove();
+  });
+
+  item.append(label, deleteButton);
+  list?.append(item);
+}
+
+function deleteTask(taskId: string) {
+  const taskIndex = tasks.findIndex(task => task.id === taskId);
+  if (taskIndex > -1) {
+    tasks.splice(taskIndex, 1);
+    saveTasks();
+  }
+}
+
+function saveTasks() {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem('tasks');
+  if (taskJSON == null) return [];
+  return JSON.parse(taskJSON);
+}
